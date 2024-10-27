f21caeba507b45851f26aa9c2354c1dfa012f7b0
27prxblms <malcondev.lds@hotmail.com>
2024-10-26 22:32:02 -0300

    new

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
 
+
+
 Route::post('users/login', [UserController::class, 'login'])->name('users.login');
 Route::post('users/register', [UserController::class, 'store'])->name('users.store');
 Route::get('/users/{user}', [UserController::class, 'show']);
